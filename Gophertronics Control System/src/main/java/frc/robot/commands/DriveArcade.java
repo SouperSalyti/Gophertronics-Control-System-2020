9b3479de03941f81e6440022d1b71b3ac0aae64d
Jesse Robinson <soupersalyti@gmail.com>
2020-01-26 13:11:09 -0600

    Changed Indentation

--- Gophertronics Control System/src/main/java/frc/robot/commands/DriveArcade.java
+++ Gophertronics Control System/src/main/java/frc/robot/commands/DriveArcade.java
@@ -13,43 +13,43 @@ import frc.robot.Robot;
 import frc.robot.RobotMap;
 
 public class DriveArcade extends Command {
-  public DriveArcade() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-    requires(Robot.m_drivetrain);
-  }
+    public DriveArcade() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(Robot.m_drivetrain);
+    }
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    // Declare and manipulate variables for movement / turning speed.
-    double moveSpeed = -Robot.m_oi.driverController.getRawAxis(RobotMap.OI_MOVE_AXIS);
-    double rotateSpeed = Robot.m_oi.driverController.getRawAxis(RobotMap.OI_ROTATE_AXIS);
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        // Declare and manipulate variables for movement / turning speed.
+        double moveSpeed = -Robot.m_oi.driverController.getRawAxis(RobotMap.OI_MOVE_AXIS);
+        double rotateSpeed = Robot.m_oi.driverController.getRawAxis(RobotMap.OI_ROTATE_AXIS);
 
     Robot.m_drivetrain.arcadeDrive(-moveSpeed, rotateSpeed);
-  }
+    }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    Robot.m_drivetrain.arcadeDrive(0, 0); // Stop the robot once joystick is within deadzone.
-  }
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        Robot.m_drivetrain.arcadeDrive(0, 0); // Stop the robot once joystick is within deadzone.
+    }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    this.end(); // go to end() if interrupted by another command.
-  }
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        this.end(); // go to end() if interrupted by another command.
+    }
 }
--- Gophertronics Control System/src/main/java/frc/robot/commands/Speedboost.java
+++ Gophertronics Control System/src/main/java/frc/robot/commands/Speedboost.java
@@ -11,40 +11,40 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class Speedboost extends Command {
-  public Speedboost() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-  }
+    public Speedboost() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-    System.out.println("Speeding");
-  }
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+        System.out.println("Speeding");
+    }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    Robot.m_drivetrain.setSpeedboost(true);
-  }
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        Robot.m_drivetrain.setSpeedboost(true);
+    }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    Robot.m_drivetrain.setSpeedboost(false);
-    System.out.println("Not Speeding");
-  }
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        Robot.m_drivetrain.setSpeedboost(false);
+        System.out.println("Not Speeding");
+    }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    this.end();
-  }
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        this.end();
+    }
 }
