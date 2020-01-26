713f55641c1ed1bca4da4bc578ee65082f8400dd
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2020-01-26 13:02:28 -0600

    Modified indentation.

--- Gophertronics Control System/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Gophertronics Control System/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -20,67 +20,57 @@ import frc.robot.commands.DriveArcade;
  * Add your docs here.
  */
 public class Drivetrain extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  
-  // Motor Controllers.
-  WPI_VictorSPX leftFrontSPX = null;
-  WPI_VictorSPX leftBackSPX = null;
-  WPI_VictorSPX rightFrontSPX = null;
-  WPI_VictorSPX rightBackSPX = null;
-
-  // Groupings for Motors.
-  SpeedControllerGroup leftDriveGroup = null;
-  SpeedControllerGroup rightDriveGroup = null;
-
-  // The Whole DriveTrain.
-  DifferentialDrive drivePlatform = null;
-
-  /* Sets whether or not arcadeDrive() utilises DRIVETRAIN_SPEED_MODIFIER 
-     or DRIVETRAIN_SPEEDBOOST_MODIFIER for motor speed. */
-  private boolean isSpeedboost;
-
-  public Drivetrain() {
-
-    // create motor objects. Each Motor is grouped into a "Drive Group" that is then placed into a "Drive Platform".    
-    this.leftFrontSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_LEFT_FRONT_ID);
-    this.leftBackSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_LEFT_BACK_ID);
-    this.leftDriveGroup = new SpeedControllerGroup(leftFrontSPX, leftBackSPX);
-
-    this.rightFrontSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_RIGHT_FRONT_ID);
-    this.rightBackSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_RIGHT_BACK_ID);
-    this.rightDriveGroup = new SpeedControllerGroup(rightFrontSPX, rightBackSPX);
     
-    this.drivePlatform = new DifferentialDrive(rightDriveGroup, leftDriveGroup);
+    // Motor Controllers, Motors, and the whole Drivetrain.
+    WPI_VictorSPX leftFrontSPX, leftBackSPX, rightFrontSPX, rightBackSPX;
+    SpeedControllerGroup leftDriveGroup, rightDriveGroup;
+    DifferentialDrive drivePlatform = null;
 
-    this.isSpeedboost = false; // False unless speedboost button is depressed.
+    /* Sets whether or not arcadeDrive() utilises DRIVETRAIN_SPEED_MODIFIER 
+        or DRIVETRAIN_SPEEDBOOST_MODIFIER for motor speed. */
+    private boolean isSpeedboost;
 
-  }
+    public Drivetrain() {
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-    setDefaultCommand(new DriveArcade());
-  }
+        // create motor objects. Each Motor is grouped into a "Drive Group" that is then placed into a "Drive Platform".    
+        this.leftFrontSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_LEFT_FRONT_ID);
+        this.leftBackSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_LEFT_BACK_ID);
+        this.leftDriveGroup = new SpeedControllerGroup(leftFrontSPX, leftBackSPX);
 
-  // Sets speed of motor controllers.
-  public void arcadeDrive(double moveSpeed, double rotateSpeed) {
-    if (this.getSpeedboost() == false) { // Checks if the speedboost trigger is enabled.
-      System.out.println("speeding");
-      this.drivePlatform.arcadeDrive(moveSpeed*RobotMap.DRIVETRAIN_SPEED_MODIFIER,rotateSpeed*RobotMap.DRIVETRAIN_SPEED_MODIFIER);
+        this.rightFrontSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_RIGHT_FRONT_ID);
+        this.rightBackSPX = new WPI_VictorSPX(RobotMap.DRIVETRAIN_RIGHT_BACK_ID);
+        this.rightDriveGroup = new SpeedControllerGroup(rightFrontSPX, rightBackSPX);
+        
+        this.drivePlatform = new DifferentialDrive(rightDriveGroup, leftDriveGroup);
+
+        this.isSpeedboost = false; // False unless speedboost button is depressed.
+
     }
-    else {
-      System.out.println("not speeding");
-      this.drivePlatform.arcadeDrive(moveSpeed*RobotMap.DRIVETRAIN_SPEEDBOOST_MODIFIER,rotateSpeed*RobotMap.DRIVETRAIN_SPEEDBOOST_MODIFIER);
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new DriveArcade());
     }
-  }
 
-  public void setSpeedboost(boolean set) {
-    this.isSpeedboost = set;
-  }
+    // Sets speed of motor controllers.
+    public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+        if (this.getSpeedboost() == false) { // Checks if the speedboost trigger is enabled.
+            System.out.println("speeding");
+            this.drivePlatform.arcadeDrive(moveSpeed*RobotMap.DRIVETRAIN_SPEED_MODIFIER,rotateSpeed*RobotMap.DRIVETRAIN_SPEED_MODIFIER);
+        } else {
+            System.out.println("not speeding");
+            this.drivePlatform.arcadeDrive(moveSpeed*RobotMap.DRIVETRAIN_SPEEDBOOST_MODIFIER,rotateSpeed*RobotMap.DRIVETRAIN_SPEEDBOOST_MODIFIER);
+        }
+    }
 
-  public boolean getSpeedboost() {
-    return this.isSpeedboost;
-  }
+    public void setSpeedboost(boolean set) {
+        this.isSpeedboost = set;
+    }
+
+    public boolean getSpeedboost() {
+        return this.isSpeedboost;
+    }
+
 }
