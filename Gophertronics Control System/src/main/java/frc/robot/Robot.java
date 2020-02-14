34cc5e1843dd480e847211128460a91d4baae54e
Jesse Robinson <soupersalyti@gmail.com>
2020-02-14 17:25:42 -0600

    Removed Example Code

--- Gophertronics Control System/src/main/java/frc/robot/Robot.java
+++ Gophertronics Control System/src/main/java/frc/robot/Robot.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.*;
 
 /**
@@ -26,7 +25,7 @@ import frc.robot.subsystems.*;
  */
 public class Robot extends TimedRobot {
 
-    public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+
     public static OI m_oi;
 
     public static Drivetrain m_drivetrain = null;
@@ -71,7 +70,6 @@ public class Robot extends TimedRobot {
 
         // Operator Input stuff.
         m_oi = new OI();
-        m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
         // chooser.addOption("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", m_chooser);
 
